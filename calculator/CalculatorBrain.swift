1fe8c42057e162236ede98bd8238830a200aadba
alinusgallus <gall.alain@gmail.com>
2015-07-19 15:38:27 +0800

    End of class 3. Ready for Project assignment 2

---
+++ calculator/CalculatorBrain.swift
@@ -0,0 +1,101 @@
+//
+//  CalculatorBrain.swift
+//  calculator
+//
+//  Created by Alain Gall on 7/17/15.
+//  Copyright (c) 2015 Alain Gall. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    enum Op: Printable
+    {
+        case Operand (Double)
+        case UnaryOperation (String, Double -> Double)
+        case BinaryOperation (String, (Double, Double) ->Double)
+        
+        var description: String{
+            get {
+                switch self{
+                case .Operand(let operand):
+                    return ("\(operand)")
+                case .UnaryOperation(let symbol, _):
+                    return ("\(symbol)")
+                case .BinaryOperation(let symbol, _):
+                    return ("\(symbol)")
+                }
+            }
+        }
+    }
+    
+    var opStack = [Op]()
+    
+    var knownOps = [String:Op]()
+    
+    init() {
+        func learnOp(op: Op){
+            knownOps[op.description] = op
+        }
+        
+        learnOp(Op.BinaryOperation("+",+))
+        learnOp(Op.BinaryOperation("×",*))
+        knownOps["÷"] = Op.BinaryOperation("÷"){$1 / $0}
+        knownOps["−"] = Op.BinaryOperation("−"){$1 - $0}
+        knownOps["√"] = Op.UnaryOperation("√" , sqrt)
+        knownOps["sin"] = Op.UnaryOperation("sin"){sin($0)}
+        knownOps["cos"] = Op.UnaryOperation("cos"){cos($0)}
+    }
+    
+    func pushOnStack (operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation (symbol:String) -> Double?{
+        if let operation = knownOps[symbol]{
+        opStack.append(operation)
+        }
+        return evaluate()
+        
+    }
+    
+    func evaluate (ops: [Op]) -> (result: Double?,remainingOps: [Op]){
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            println("\(op)")
+            switch op{
+            case .Operand(let operand):
+                return (operand,remainingOps)
+            case .UnaryOperation( _ , let operation):
+                var operandEvaluation = evaluate( remainingOps)
+                if let operand = operandEvaluation.result{
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_,let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let op1 = op1Evaluation.result {
+                    
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    
+                    if let op2 = op2Evaluation.result {
+                        println("\(op1) and \(op2)")
+                        return (operation(op1,op2), op2Evaluation.remainingOps)
+                        
+                    }
+                }
+            }
+          
+        }
+        return (nil,ops)
+        
+    }
+    
+    func evaluate() -> Double {
+        let (result,remainder) = evaluate(opStack)
+        return result!
+    }
+
+}
--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,16 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var display: UILabel!
+    var userIsTyping = false
+    var brain = CalculatorBrain()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        if (!(sender.currentTitle! == "." && (display.text!.rangeOfString(".") != nil) )){
+            var digit: String
+            if (sender.currentTitle! == "π"){
+                digit = "\(M_PI)"
+            }
+            else {
+                digit = sender.currentTitle!
+                
+            }
+        
+        if userIsTyping {
+          display.text = display.text! + digit
+          userIsTyping = true
+        }
+        else {
+            display.text = digit
+            userIsTyping = true
+        }
+        }
     }
-
+    
+    
+   
+    @IBAction func enter() {
+        userIsTyping = false
+        if let result = brain.pushOnStack(displayValue){
+            displayValue = result
+        } else{
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsTyping {
+            enter()
+        }
+        if let operation = sender.currentTitle{
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+        
+      
+        enter()
+        
+        }
+    
+    @IBAction func clear(sender: UIButton) {
+        
+    }
+    
+    
+   
+  
 
 }
+
 
